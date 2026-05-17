b9c2e964c0c06250d0cf676967e74c9fe25091bc
jaehyo0206-droid <jaehyo0206@gmail.com>
2026-05-17 15:33:42 +0900

    [level 0] Title: 약수 구하기, Time: 0.01 ms, Memory: 3.94 MB -BaekjoonHub

--- 프로그래머스/0/120897. 약수 구하기/약수 구하기.cpp
+++ 프로그래머스/0/120897. 약수 구하기/약수 구하기.cpp
@@ -1,12 +1,18 @@
 #include <string>
 #include <vector>
-
+#include <cmath>
+#include <algorithm>
 using namespace std;
-
 vector<int> solution(int n) {
     vector<int> answer;
-    for(int i=1;i<=n;i++){
-        if(n%i==0)answer.push_back(i);
+    for(int i=1; i<=sqrt(n);i++){
+        if(n%i==0){
+            answer.push_back(i);
+            if(i!=sqrt(n)){
+                answer.push_back(n/i);
+            }
+        }
     }
+    sort(answer.begin(),answer.end());
     return answer;
 }

56135c723918cd70c0b6042588bb147bf271ef14
jaehyo0206-droid <jaehyo0206@gmail.com>
2026-06-07 23:01:39 +0900

    [level 0] Title: 종이 자르기, Time: 0.01 ms, Memory: 4.07 MB -BaekjoonHub

--- 프로그래머스/0/120922. 종이 자르기/종이 자르기.cpp
+++ 프로그래머스/0/120922. 종이 자르기/종이 자르기.cpp
@@ -4,6 +4,9 @@
 using namespace std;
 
 int solution(int M, int N) {
-    int answer = (N-1)*M+(M-1);
-    return answer;
+    int answer = (N-1)*M+(M-1);//MN-M+M-1 => MN-1
+    //N-1 N의 길이를 만들기 위해 필요한 가위질 수
+    //M을 만드는 길이 만큼 N-1의 가위질의 횟수가 늘어난다
+    //M-1 길이 M을 만드는데 필요한 가위질 횟수
+    return M*N-1;
 }
